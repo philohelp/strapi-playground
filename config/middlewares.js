8c759a94b0b56b903b1e57099123ccea01a9fb7b
Thomas Poirier <thpoirier@gmail.com>
2022-01-16 11:06:22 +0100

    added components

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,16 +4,27 @@ module.exports = [
     name: "strapi::security",
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
         },
       },
     },
   },
+  // {
+  //   name: "strapi::security",
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         "connect-src": ["'self'", "https:"],
+  //         "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+  //         "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
   {
     name: "strapi::cors",
     config: {
