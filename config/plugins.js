61c4bec7378d8ec031b9e84e236c0b6b51a1b4bc
Thomas Poirier <thpoirier@gmail.com>
2022-02-02 19:59:57 +0100

    added algolia

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,29 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  });
+  },
+  search: {
+    enabled: true,
+    config: {
+      provider: "algolia",
+      providerOptions: {
+        apiKey: env("ALGOLIA_PROVIDER_ADMIN_API_KEY"),
+        applicationId: env("ALGOLIA_PROVIDER_APPLICATION_ID"),
+      },
+      contentTypes: [
+        { name: "api::famille-de-metier.famille-de-metier", index: "main" },
+      ],
+    },
+  },
+});
